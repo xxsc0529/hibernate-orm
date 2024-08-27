6afebb491b105d59c6ccd82df784237f4c0c6635
zyh <xxsc0529@163.com>
2024-08-27 17:11:40 +0800

    feature:test oceanbase

--- ci/database-start.sh
+++ ci/database-start.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ "$RDBMS" == 'mysql' ]; then
-  bash $DIR/../docker_db.sh mysql
+  bash $DIR/../docker_db.sh oceanbase
 elif [ "$RDBMS" == 'oceanbase' ]; then
   bash $DIR/../docker_db.sh oceanbase
 elif [ "$RDBMS" == 'mariadb' ]; then
